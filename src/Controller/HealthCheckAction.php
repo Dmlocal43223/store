ee2452d7fc497b0f4b8c4385a58d8711526ec054
Evgeniy Khudyaev <ivanovnew119@gmail.com>
2024-04-29 23:30:31 +0300

    add debug and phpunit

---
+++ src/Controller/HealthCheckAction.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/health-check', name: 'healthCheck', methods: ['GET'])]
+class HealthCheckAction
+{
+    public function __invoke(): Response
+    {
+        Return new JsonResponse(['status' => 'ok']);
+    }
+}
